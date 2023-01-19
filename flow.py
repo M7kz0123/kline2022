6480469f663a1bbf65e31c77cf3a8e79b44f0e2e
M7kz0123 <mikezhou0123@gmail.com>
2023-01-19 20:24:02 +0900

    git pull git add . git commit -m"add by zhou" git push

--- flow.py
+++ flow.py
@@ -1,6 +1,5 @@
 import gurobipy as gp
 
-    with open("small.dat", 'r') as f:
 
 with open("small.dat", 'r') as f:
     lines = [line.rstrip() for line in f.readlines()]
@@ -13,6 +12,7 @@ V = {i for i in range(1,n+1)}
 V_p = {i for i in range(0,n+2)}
 items = lines.pop(0).split(' ')
 p = {i+1: int(v) for i, v in enumerate(items)}
+p[m+1] = 1
 items = lines.pop(0).split(' ')
 o = {i+1: int(v) for i, v in enumerate(items)}
 items = lines.pop(0).split(' ')
@@ -37,26 +37,57 @@ x.update({(i,m+1): 0 for i in V})
 x[n+1,m+1] = 1
 alpha = {(i,j): model.addVar(vtype = gp.GRB.BINARY, name = "a[{},{}]".format(i,j)) for (i,j) in E}
 beta = {(i,j): model.addVar(vtype = gp.GRB.BINARY, name = "b[{},{}]".format(i,j)) for (i,j) in E_b}
+
+u = {(i,j): model.addVar(vtype = gp.GRB.CONTINUOUS, lb = 0, name = "u[{},{}]".format(i,j)) for (i,j) in E}
+v = {(i,j): model.addVar(vtype = gp.GRB.CONTINUOUS, lb = 0, name = "v[{},{}]".format(i,j)) for (i,j) in E_b}
 model.update()
 
 # 目標関数
 
 
 # 制約関数
-model.addConstrs((gp.quicksum(q[i] * x[i,k] for i in V) >= p[k] for k in M), name="(34)")
-model.addConstrs((gp.quicksum(x[i,k] for k in M) == 1 for i in V), name="(35)")
+model.addConstrs((gp.quicksum(q[i] * x[i,k] for i in V) >= p[k] for k in M), name="(18)")
+model.addConstrs((gp.quicksum(x[i,k] for k in M) == 1 for i in V), name="(19)")
 model.addConstrs((gp.quicksum(o[k] * x[j,k] for k in M_p) <= gp.quicksum(o[k] * x[i,k] for k in M_p) + o_max * (1-alpha[i,j]) 
-                                for (i,j) in E if i != 0), name="(36)")
+                                for (i,j) in E if i != 0), name="(20)")
 model.addConstrs((gp.quicksum(d[k] * x[j,k] for k in M_p) <= gp.quicksum(d[k] * x[i,k] for k in M_p) + d_max * (1-beta[i,j])
-                                for (i,j) in E_b if j != 0), name="(37)")
-model.addConstrs((gp.quicksum(alpha[j,i] for j in V_p if (j,i) in E) == 1 for i in V | {n+1}), name="(38a)")
-model.addConstrs((gp.quicksum(beta[i,j] for j in V_p if (i,j) in E_b) == 1 for i in V | {n+1}), name="(38b)")
+                                for (i,j) in E_b if j != 0), name="(21)")
+
+model.addConstrs((gp.quicksum(u[j,i] for j in V_p if (j,i) in E) - gp.quicksum(u[i,j] for j in V_p if (i,j) in E) == gp.quicksum(q[i] * x[i,k] for k in M_p)
+                                for i in V | {n+1}), name = "(22)")
+model.addConstr((gp.quicksum(u[0,j] for j in V_p if (0,j) in E) == gp.quicksum(p[k] for k in M_p)), name = "(23)") 
+model.addConstrs((gp.quicksum(v[i,j] for j in V_p if (i,j) in E_b) - gp.quicksum(v[j,i] for j in V_p if (j,i) in E_b) == gp.quicksum(q[i] * x[i,k]for k in M_p)
+                                for i in V | {n+1}), name = "(24)")
+model.addConstr((gp.quicksum(v[i,0] for i in V_p if (i,0) in E_b) == gp.quicksum(p[k] for k in M_p)), name = "(25)")
+
+model.addConstrs((u[j,i] <= q_sum * alpha[j,i] for (j,i) in E), name = "(26b)")
+
+model.addConstrs((v[i,j] <= q_sum * beta[i,j] for (i,j) in E_b), name = "(27b)")
+model.addConstrs((gp.quicksum(alpha[j,i] for j in V_p if (j,i) in E) == 1 for i in V | {n+1}), name="(28a)")
+model.addConstrs((gp.quicksum(beta[i,j] for j in V_p if (i,j) in E_b) == 1 for i in V | {n+1}), name="(28b)")
+
+
 
 # TBD
 # 1.向きの対応
 # 2.高さ制限の対応
 # 3.元のグラフに閉路が存在する場合，さらに部分巡回路を排除する制約の追加が必要
+
+
+
+model.update()
+model.write("a.lp")
+
 model.optimize()
+model.write("solution.sol")
+
+#if model.status == gp.GRB.OPTIMAL:
+#    model.printAttr('x')
+#if model.solCount == 0:
+    #print("Model is infeasible")
+    #model.computeIIS()
+    #model.write("model_iis.ilp")
+
 
 sol = {i: k for i,k in x if isinstance(x[i,k], gp.Var) and  x[i,k].x > 0.5}
 print(sol)
@@ -65,4 +96,3 @@ sola = {(i,k) for i,k in alpha if isinstance(alpha[i,k], gp.Var) and  alpha[i,k]
 print(sola)
 solb = {(i,k) for i,k in beta if isinstance(beta[i,k], gp.Var) and  beta[i,k].x > 0.5}
 print(solb)
-#print(o[m+1])
